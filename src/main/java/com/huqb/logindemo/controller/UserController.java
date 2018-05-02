11294815a23a9d7c34c03fe42cc49a4affb079a3
wx-huqb <wx-huqb@dtdream.com>
2018-05-02 11:57:31 +0800

    返回结果增加作者

--- src/main/java/com/huqb/logindemo/controller/UserController.java
+++ src/main/java/com/huqb/logindemo/controller/UserController.java
@@ -5,6 +5,7 @@ import com.huqb.logindemo.result.ResultBack;
 import com.huqb.logindemo.result.ResultImpl;
 import com.huqb.logindemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +17,28 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Value("${author.name}")
+    private String author_name;
+    @Value("${author.age}")
+    private Integer author_age;
+    @Value("${author.sex}")
+    private String author_sex;
+
+
     @RequestMapping("login/{id}")
     @ResponseBody
     public ResultBack getUserById(@PathVariable int id){
 
         Person person = userService.selectPersonById(id);
+
         if(null == person){
             return ResultImpl.error("400001","没有这个人",null);
         }else{
-            return ResultImpl.success("200001","找到对应信息",person);
+            ResultBack resultBack = ResultImpl.success("200001","找到对应信息",person);
+            resultBack.setAuthor_name(author_name);
+            resultBack.setAuthor_age(author_age);
+            resultBack.setAuthor_sex(author_sex);
+            return resultBack;
         }
 
     }
--- src/main/java/com/huqb/logindemo/result/ResultBack.java
+++ src/main/java/com/huqb/logindemo/result/ResultBack.java
@@ -6,6 +6,10 @@ public class ResultBack {
     private String return_msg;
     private Object data;
 
+    private String author_name;
+    private Integer author_age;
+    private String author_sex;
+
     public String getReturn_code() {
         return return_code;
     }
@@ -28,5 +32,29 @@ public class ResultBack {
 
     public void setData(Object data) {
         this.data = data;
+    }
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+
+    public Integer getAuthor_age() {
+        return author_age;
+    }
+
+    public void setAuthor_age(Integer author_age) {
+        this.author_age = author_age;
+    }
+
+    public String getAuthor_sex() {
+        return author_sex;
+    }
+
+    public void setAuthor_sex(String author_sex) {
+        this.author_sex = author_sex;
     }
 }
